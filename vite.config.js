8f63acd015fb64939346d081e8deb28774c66a4b
kiran <kiran.padala@sasi.ac.in>
2026-05-28 23:26:12 +0530

    fixes in files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/breathe-esg-dashboard-frontend/',
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
